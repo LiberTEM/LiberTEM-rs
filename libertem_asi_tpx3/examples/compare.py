9ee615236de13975141dac3e4e3d3969f584d20c
Alexander Clausen <alex@gc-web.de>
2023-02-21 19:58:20 +0100

    Split off comparison from simple example

--- libertem_asi_tpx3/examples/compare.py
+++ libertem_asi_tpx3/examples/compare.py
@@ -7,6 +7,26 @@ import numpy as np
 from libertem_asi_tpx3 import ASITpx3Connection, CamClient, ChunkStackHandle
 
 
+def get_reference_array(fn):
+    with open(fn, "rb") as f:
+        meta = tomli.load(f)
+
+    dirname = os.path.dirname(fn)
+    indices = os.path.join(dirname, meta["raw_csr"]["indices_file"])
+    indptr = os.path.join(dirname, meta["raw_csr"]["indptr_file"])
+    data = os.path.join(dirname, meta["raw_csr"]["data_file"])
+    
+    indptr_arr = np.memmap(indptr, dtype=meta["raw_csr"]["indptr_dtype"], mode="r")
+    indices_arr = np.memmap(indices, dtype=meta["raw_csr"]["indices_dtype"], mode="r")
+    data_arr = np.memmap(data, dtype=meta["raw_csr"]["data_dtype"], mode="r")
+
+    arr = scipy.sparse.csr_matrix(
+        (data_arr, indices_arr, indptr_arr),
+        shape=(int(np.prod(meta["params"]["nav_shape"])), int(np.prod(meta["params"]["sig_shape"]))),
+    )
+    return arr
+
+
 if __name__ == "__main__":
     conn = ASITpx3Connection(
         uri="localhost:8283",
@@ -21,8 +41,11 @@ if __name__ == "__main__":
     conn.serve_shm(sock)
     conn.start_passive()
 
+    reference_array = get_reference_array("/cachedata/alex/tpx3/csr_streaming/sparse.toml")
+    print(reference_array)
+
     split_at = 16*512
-    # split_at = 128
+    split_at = 128
 
     for i in range(100):
         while (header := conn.wait_for_arm(timeout=1)) is None:
@@ -51,6 +74,13 @@ if __name__ == "__main__":
                     shape=(layout.get_nframes(), int(np.prod((516, 516)))),
                 )
                 seen += 1
+
+                ref_slice = reference_array[frames_cursor:frames_cursor + layout.get_nframes(), :]
+                assert ref_slice.nnz == chunk_arr.nnz
+                assert np.allclose(ref_slice.data, chunk_arr.data)
+                assert np.allclose(ref_slice.indices, chunk_arr.indices)
+                assert np.allclose(ref_slice.indptr, chunk_arr.indptr)
+                print("allclose!")
 
                 frames_cursor += layout.get_nframes()
 
