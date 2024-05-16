0d4fb67036d412d0341d7cd8405308dccf6a7a35
Alexander Clausen <alex@gc-web.de>
2024-05-16 13:49:58 +0200

    WIP: generic frame iterator/receiver infra
    Doesn't build yet, very WIP

--- common/src/generic_receiver.rs
+++ common/src/generic_receiver.rs
@@ -1,3 +1,19 @@
+use std::time::Duration;
+
+use crate::frame_stack::{FrameMeta, FrameStackHandle};
+
+/// Messages from the background thread to the foreground code
+#[derive(PartialEq, Eq, Debug)]
+pub enum ReceiverMsg<M: FrameMeta> {
+    /// A frame stack has been received and is ready for processing
+    FrameStack {
+        frame_stack: FrameStackHandle<M>,
+    },
+
+    /// 
+    Stuff,
+}
+
 pub enum ReceiverStatus {
     Initializing,
     Idle,
@@ -10,6 +26,10 @@ pub enum ReceiverStatus {
     Closed,
 }
 
-pub trait Receiver {
+pub trait Receiver<M: FrameMeta> {
     fn get_status(&self) -> ReceiverStatus;
+
+    /// Get the next message from the background thread,
+    /// waiting at most `timeout`; returns `None` if no the timeout was hit.
+    fn next_timeout(&mut self, timeout: Duration) -> Option<ReceiverMsg<M>>;
 }
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,6 @@
-pub mod frame_chunked_iterator;
+pub mod frame_iterator;
 pub mod frame_stack;
-pub mod frame_stack_py;
+pub mod py_frame_stack;
 pub mod generic_receiver;
 pub mod stats;
+pub mod py_frame_iterator;
