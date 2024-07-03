b9ed660160d6726b5dabe0faa7f2023c419b5b2c
Alexander Clausen <alex@gc-web.de>
2024-07-03 17:36:14 +0200

    Misc. fixes for dectris: LiberTEM-live tests pass
    * Fix stats: properly count the last frame stack
    * Fix `impl_py_connection` close implementation to actually
      `Option::take` the connection implementation and call `close` on it
    * Add a lot of debug logging to make it easier to diagnose issues

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,7 +4,6 @@ pub mod decoder;
 pub mod frame_stack;
 pub mod generic_connection;
 pub mod generic_receiver;
-pub mod py_buffer_for_slot;
 pub mod py_cam_client;
 pub mod py_connection;
 pub mod stats;
