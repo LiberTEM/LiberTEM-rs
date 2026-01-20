07fbce28b084961c3998104baaea1c20b04600cc
Alexander Clausen <a.clausen@fz-juelich.de>
2026-01-20 19:48:44 +0100

    update-licenses.sh: keep custom licenses intact

--- update-licenses.sh
+++ update-licenses.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for crate in libertem_dectris libertem_asi_tpx3 libertem_asi_mpx3 libertem_qd_mpx; do
-  (cd $crate && cargo bundle-licenses --format yaml --output THIRDPARTY.yml);
+  (cd $crate && cargo bundle-licenses --format yaml --output THIRDPARTY.yml --previous THIRDPARTY.yml);
 done
